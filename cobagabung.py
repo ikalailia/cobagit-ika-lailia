6ee9f2afb7650b257b47934a898170b686e77af7
ikalailia <20611106@students.uii.ac.id>
2023-06-15 20:15:02 +0700

    menambhakan untuk my love

--- cobagabung.py
+++ cobagabung.py
@@ -1,1 +1,80 @@
 #ika lailia cuantik
+
+
+Memanggil namamu ke ujung dunia 
+
+Tiada yang lebih pilu 
+
+Tiada yang menjawabku selain hatiku
+
+ 
+
+Dan ombak berderu 
+
+Di pantai ini kau slalu sendiri 
+
+Tak ada jejakku di sisimu 
+
+Namun saat ku tiba 
+
+Suaraku memanggilmu akulah lautan 
+
+ 
+
+Ke mana kau s'lalu pulang 
+
+Jingga di bahuku 
+
+Malam di depanku 
+
+Dan bulan siaga sinari langkahku 
+
+ 
+
+Ku terus berjalan 
+
+Ku terus melangkah
+
+Kuingin kutahu engkau ada 
+
+Memandangimu saat senja
+
+Berjalan di batas dua dunia
+
+ 
+
+Tiada yang lebih indah
+
+Tiada yang lebih rindu
+
+Selain hatiku
+
+Andai engkau tahu
+
+Di pantai itu kau tampak sendiri
+
+Tak ada jejakku di sisimu
+
+ 
+
+Namun saat kau rasa
+
+Pasir yang kau pijak pergi akulah lautan
+
+Memeluk pantaimu erat
+
+Jingga di bahumu
+
+Malam di depanmu
+
+ 
+
+Dan bulan siaga sinari langkahmu
+
+Teruslah berjalan
+
+Teruslah melangkah
+
+Ku tahu kau tahu aku ada
+
+#from mumtaz your love
